09cfbecd30d61d3d0aa3b6f05953fb2c7ae362d4
AndresMpa <andresprieto2703@gmail.com>
2021-04-30 16:19:35 -0500

    Better installation file

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+instalation_dir=~/.config/nvim
+
 cd ..
 mv nvim-configuration nvim
 
+echo "Are you using a custom config dir? (Default ~/.config)[y/n]: "
+read custom_dir
+
+if [ $custom_dir == "y" ];
+then
+	echo "Write your custom dir name: "
+	read instalation_dir
+fi
+	
 if [ -e ~/.config/nvim ];
 then
 	mv ~/.config/nvim old-nvim
-	mv $(pwd) ~/.config/nvim
+	mv $(pwd) $instalation_dir
 else
 	if [ -e ~/.config ];
 	then
-		mv nvim ~/.config/nvim
-	fi
+		mv nvim $instalation_dir
+	else
 		mkdir ~/.config
-		mv nvim ~/.config/nvim
+		mv nvim $instalation_dir
+	fi
 fi
 
 echo "
