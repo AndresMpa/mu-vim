8caa43401c9ca57fea96b26398cc28e09b10fbea
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-12 09:13:41 -0500

    Image Previewer
    Adding configuration files

---
+++ lua/setUp/imagePreviwer.lua
@@ -0,0 +1,13 @@
+-- Require and call setup function somewhere in your init.lua
+require('image').setup {
+  render = {
+    min_padding = 5,
+    show_label = true,
+    use_dither = true,
+    foreground_color = true,
+    background_color = true
+  },
+  events = {
+    update_on_nvim_resize = true,
+  },
+}
