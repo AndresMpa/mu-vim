d56cca4b394094a7092f007e9305bdc1cf668bfa
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-12 09:17:25 -0500

    Extended support
    Adding some extra file types

--- lua/lsp/dictionary.lua
+++ lua/lsp/dictionary.lua
@@ -3,6 +3,7 @@ require("cmp_dictionary").setup({
 		--I removing this support for every possibility, I found it annoying
 		--["*"] = "~/.config/nvim/dicts/english.dict",
 		filepath = {
+			["COMMIT_EDITMSG"] = "~/.config/nvim/dicts/english.dict",
 			["*%.html"] = "~/.config/nvim/dicts/english.dict",
 			["*%.txt"] = "~/.config/nvim/dicts/english.dict",
 			["*%.md"] = "~/.config/nvim/dicts/english.dict",
