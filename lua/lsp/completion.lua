f82726aadf9a58ab2a214f81ad670613f9c39738
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-13 05:07:53 -0500

    Minor changes

--- lua/lsp/completion.lua
+++ lua/lsp/completion.lua
@@ -67,10 +67,13 @@ cmp.setup({
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = function(entry, vim_item)
-			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+			local kind = require("lspkind").cmp_format({
+				mode = "symbol_text",
+				maxwidth = 50,
+			})(entry, vim_item)
 			local strings = vim.split(kind.kind, "%s", { trimempty = true })
-			kind.kind = " " .. strings[1] .. " "
-			kind.menu = "    (" .. strings[2] .. ")"
+			kind.kind = " " .. (strings[1] or "") .. " "
+			kind.menu = "    (" .. (strings[2] or "") .. ")"
 
 			return kind
 		end,
