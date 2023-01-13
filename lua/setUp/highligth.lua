21de85723f54864793043a55b3cfd893098369bf
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-13 05:01:05 -0500

    Auto tags
    Enabling auto tags

--- lua/setUp/highligth.lua
+++ lua/setUp/highligth.lua
@@ -5,6 +5,10 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 
+	autotag = {
+		enable = true,
+	},
+
 	indent = {
 		enable = false,
 		disable = {},
