6e54ff707ee2a82a1c3713b76f14f11e477e28a0
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-23 10:14:46 -0500

    Rome (LSP)
    Moving form prettier to Rome to test some Rust

--- lua/lsp/formatter.lua
+++ lua/lsp/formatter.lua
@@ -10,24 +10,27 @@ require("formatter").setup({
 	-- All formatter configurations are opt-in
 	filetype = {
 		sh = { require("formatter.filetypes.sh").shfmt },
-		lua = { require("formatter.filetypes.lua").stylua },
-		css = { require("formatter.filetypes.css").prettier },
+
+		css = { require("formatter.filetypes.css").rome },
+		html = { require("formatter.filetypes.html").rome },
+		json = { require("formatter.filetypes.json").rome },
+		markdown = { require("formatter.filetypes.markdown").rome },
+		javascript = { require("formatter.filetypes.javascript").rome },
+		typescript = { require("formatter.filetypes.typescript").rome },
+
 		scss = { require("formatter.filetypes.css").prettier },
 		less = { require("formatter.filetypes.css").prettier },
-		html = { require("formatter.filetypes.html").prettier },
-		json = { require("formatter.filetypes.json").prettier },
 		yaml = { require("formatter.filetypes.yaml").prettier },
-		python = { require("formatter.filetypes.python").autopep8 },
 		svelte = { require("formatter.filetypes.svelte").prettier },
 		vue = { require("formatter.filetypes.javascript").prettier },
 		graphql = { require("formatter.filetypes.graphql").prettier },
-		markdown = { require("formatter.filetypes.markdown").prettier },
 		angular = { require("formatter.filetypes.javascript").prettier },
-		javascript = { require("formatter.filetypes.javascript").prettier },
-		typescript = { require("formatter.filetypes.typescript").prettier },
 		javascriptreact = { require("formatter.filetypes.javascriptreact").prettier },
 		typescriptreact = { require("formatter.filetypes.typescriptreact").prettier },
 
+		python = { require("formatter.filetypes.python").autopep8 },
+
+		lua = { require("formatter.filetypes.lua").stylua },
 		["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
 	},
 })
