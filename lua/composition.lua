1ad00d27660139bde375ad7bdd5a6ef08b45a89d
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-24 01:07:40 -0500

    MLD
    Using a easier way to handle with set up files

--- lua/composition.lua
+++ lua/composition.lua
@@ -1,32 +1,13 @@
 -- SET UP
 
--- AUTO SAVE
-require("setUp.autosave")
-
--- GREETER
-require("setUp.greeter")
-
--- THEME
--- Nvim
-require("scheme.material")
--- Bar
-require("setUp.statusLine")
-
--- VISUAL HELP
--- Identation
-require("setUp.identation")
--- Spell
-require("setUp.spelling")
--- Highlight
-require("setUp.highligth")
--- Highlighter (theme)
-require("colorizer").setup()
-
--- NAVIGATION
--- File manager
-require("setUp.fileManager")
--- Multitab
-require("setUp.buffer")
-
 -- LSP
 require("lsp")
+
+-- MAPPING
+require("mapping")
+
+-- MODES
+require("modes")
+
+-- VISUAL HELP
+require("setUp")
---
+++ lua/setUp/init.lua
@@ -0,0 +1,28 @@
+-- GREETER
+require("setUp.greeter")
+
+-- THEME
+-- Nvim
+require("scheme.material")
+-- Bar
+require("setUp.statusLine")
+
+-- Highlight
+require("setUp.highligth")
+-- Highlighter (theme)
+require("colorizer").setup()
+
+-- CODE
+-- Identation
+require("setUp.identation")
+-- Spell
+require("setUp.spelling")
+
+-- NAVIGATION
+-- File manager
+require("setUp.fileManager")
+-- Multitab
+require("setUp.buffer")
+
+-- AUTO SAVE
+require("setUp.autosave")
