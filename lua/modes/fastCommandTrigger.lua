eeabac38dda2ec8dec3fccd2dbd3d3ddb5ab9e00
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-24 18:35:36 -0500

    FCT
    Looking for something useful to do whit this features

--- lua/modes/fastCommandTrigger.lua
+++ lua/modes/fastCommandTrigger.lua
@@ -1,10 +1,24 @@
+local execute = vim.api.nvim_command
 local libmodal = require("libmodal")
 
-local modes = {
-	j = "split",
-	k = "vsplit",
+local fct = {
+	f = function()
+		execute(":PackerInstall")
+	end,
+	d = function()
+		execute(":PackerClean")
+	end,
+	s = function()
+		execute(":PackerSync")
+	end,
+	v = function()
+		execute(":Mason")
+	end,
+	c = function()
+		execute(":MasonLog")
+	end,
 }
 
 vim.keymap.set("n", "mmf", function()
-	libmodal.mode.enter("FCT", modes)
+	libmodal.mode.enter("FAST-COMMAND-TRIGGER", fct)
 end, {})
