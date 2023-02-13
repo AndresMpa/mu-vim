0c3867b371fd93d5f404df8e6e647ac3325bb456
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-13 06:14:12 -0500

    Quick fix
    Just removing a space

--- lua/setUp/finder.lua
+++ lua/setUp/finder.lua
@@ -2,7 +2,6 @@ local trouble = require("trouble.providers.telescope")
 local actions = require("telescope.actions")
 local telescope = require("telescope")
 
-
 telescope.setup({
 	defaults = {
 		mappings = {
