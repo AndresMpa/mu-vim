1165803b7ce7bfe3f6968ce83a1f43b9a730d7d5
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-13 11:59:39 -0500

    Getting features back
    After reading ref, I found a way to auto open directories as It used to be
    
    Ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup#open-for-directories-and-change-neovims-directory

--- lua/setUp/fileManager.lua
+++ lua/setUp/fileManager.lua
@@ -1,12 +1,9 @@
 require("nvim-tree").setup({
 	disable_netrw = true,
 	hijack_netrw = true,
-  open_on_setup_file = false,
-	open_on_setup = false,
 	open_on_tab = false,
 	hijack_cursor = false,
 	update_cwd = false,
-	ignore_ft_on_setup = {},
 
 	diagnostics = {
 		enable = true,
@@ -34,40 +31,57 @@ require("nvim-tree").setup({
 		custom = {},
 	},
 
-  view = {
-    width = 30,
-    side = "left",
-    adaptive_size = true,
-    hide_root_folder = false,
-    centralize_selection = false,
-    preserve_window_proportions = false,
-    relativenumber = false,
-    number = false,
-    signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {},
-    },
-  },
+	view = {
+		width = 30,
+		side = "left",
+		adaptive_size = true,
+		hide_root_folder = false,
+		centralize_selection = false,
+		preserve_window_proportions = false,
+		relativenumber = false,
+		number = false,
+		signcolumn = "yes",
+		mappings = {
+			custom_only = false,
+			list = {},
+		},
+	},
 
-  renderer = {
-    full_name = false,
-    group_empty = false,
-    add_trailing = false,
-    highlight_git = false,
-    highlight_opened_files = "none",
-    root_folder_modifier = ":~",
-    indent_width = 2,
-    indent_markers = {
-      enable = false,
-      inline_arrows = true,
-      icons = {
-        corner = "└",
-        edge = "│",
-        item = "│",
-        bottom = "─",
-        none = " ",
-      },
-    },
-  },
+	renderer = {
+		full_name = false,
+		group_empty = false,
+		add_trailing = false,
+		highlight_git = false,
+		highlight_opened_files = "none",
+		root_folder_modifier = ":~",
+		indent_width = 2,
+		indent_markers = {
+			enable = false,
+			inline_arrows = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				bottom = "─",
+				none = " ",
+			},
+		},
+	},
 })
+
+local function open_nvim_tree(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- change to the directory
+	vim.cmd.cd(data.file)
+
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
