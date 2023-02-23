af346b6ebd65efd3b1ed2b3bc17edf33baf1d7be
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-23 01:09:37 -0500

    Data science support
    Adding support for DVC

--- lua/autocommand.lua
+++ lua/autocommand.lua
@@ -1,13 +1,18 @@
 --Packer
 vim.cmd([[
-    augroup packer_user_config
-        autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-    augroup end
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+-- DVC
+vim.cmd([[
+  autocmd! BufNewFile,BufRead Dvcfile,*.dvc,dvc.lock setfiletype yaml
 ]])
 
 vim.cmd([[
-    au BufEnter * set fo-=c fo-=r fo-=o
+  au BufEnter * set fo-=c fo-=r fo-=o
 ]])
 
 --Syntax
