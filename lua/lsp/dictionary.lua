bd08634013cca85f309cedb7a0f6e88c75e3f283
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-27 01:32:43 -0500

    Getting dictionaries back
    After reading last published request, getting back dictionaries

--- lua/lsp/dictionary.lua
+++ lua/lsp/dictionary.lua
@@ -1,15 +1,18 @@
-require("cmp_dictionary").setup({
-	dic = {
-		--I removing this support for every possibility, I found it annoying
-		--["*"] = "~/.config/nvim/dicts/english.dict",
-		filepath = {
-			["COMMIT_EDITMSG"] = "~/.config/nvim/dicts/english.dict",
-			["*%.html"] = "~/.config/nvim/dicts/english.dict",
-			["*%.txt"] = "~/.config/nvim/dicts/english.dict",
-			["*%.md"] = "~/.config/nvim/dicts/english.dict",
-		},
-		spelllang = {
-			--en = "/path/to/en.dict",
-		},
+require("cmp_dictionary").switcher({
+	--I removing this support for every possibility, I found it annoying
+	--["*"] = "~/.config/nvim/dicts/english.dict",
+	filetype = {
+		markdown = "~/.config/nvim/dicts/english.dict",
+		html = "~/.config/nvim/dicts/english.dict",
+		text = "~/.config/nvim/dicts/english.dict",
+	},
+	filepath = {
+		["COMMIT_EDITMSG"] = "~/.config/nvim/dicts/english.dict",
+		["*%.html"] = "~/.config/nvim/dicts/english.dict",
+		["*%.txt"] = "~/.config/nvim/dicts/english.dict",
+		["*%.md"] = "~/.config/nvim/dicts/english.dict",
+	},
+	spelllang = {
+		--en = "/path/to/en.dict",
 	},
 })
