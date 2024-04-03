32ecd14a474a7be04eacb533caad75224f6319d6
Andres M. Prieto <andresprieto2703@gmail.com>
2024-04-03 01:41:37 -0500

    Fix ibl
    Fixing indent_blankline for migration from v2 to v3

--- lua/setUp/identation.lua
+++ lua/setUp/identation.lua
@@ -5,29 +5,51 @@ vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
-require("indent_blankline").setup({
-	char = "▏",
-	space_char_blankline = " ",
-	show_first_indent_level = true,
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
-	filetype_exclude = {
-		"help",
-		"git",
-		"markdown",
-		"text",
-		"terminal",
-		"lspinfo",
-		"packer",
-	},
-	buftype_exclude = {
-		"terminal",
-		"nofile",
-	},
+require("ibl").setup({
+  scope = {
+    char = "_",
+    enabled = true,
+    show_end = true,
+    show_start = true,
+    show_exact_scope = true,
+    injected_languages = true,
+    highlight = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+      "IndentBlanklineIndent3",
+      "IndentBlanklineIndent4",
+      "IndentBlanklineIndent5",
+      "IndentBlanklineIndent6",
+    },
+  },
+  indent = {
+    char = "▏",
+    smart_indent_cap = true,
+    highlight = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+      "IndentBlanklineIndent3",
+      "IndentBlanklineIndent4",
+      "IndentBlanklineIndent5",
+      "IndentBlanklineIndent6",
+    },
+  },
+  exclude = {
+    filetypes = {
+      "git",
+      "man",
+      "help",
+      "text",
+      "packer",
+      "lspinfo",
+      "markdown",
+      "terminal",
+      "gitcommit",
+      "checkhealth",
+    },
+    buftypes = {
+      "nofile",
+      "terminal",
+    },
+  },
 })
