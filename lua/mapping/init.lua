fc22fb29e8903b91b103c0856a28fb651483764e
Andres M. Prieto <andresprieto2703@gmail.com>
2024-04-03 02:29:55 -0500

    Diagnostics
    Tracking diagnostics changes for toggle function

--- lua/mapping/init.lua
+++ lua/mapping/init.lua
@@ -7,14 +7,14 @@ require("mapping.basis")
 -- GIT MANAGEMENT
 require("mapping.git")
 
--- LSP
-require("mapping.lsp")
-
 -- CUSTOM
 require("mapping.custom")
 
 -- NAVIGATION THROUGH VIM
 require("mapping.navigation")
+
+-- DIAGNOSTICS
+require("mapping.diagnostics")
 
 -- DEPENDENCIES MANAGEMENT
 require("mapping.packageManager")
