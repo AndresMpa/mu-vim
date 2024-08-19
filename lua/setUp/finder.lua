2673554a51506efb75fc3ec6dac1c96f6d031217
Andres M. Prieto <andresprieto2703@gmail.com>
2024-08-19 17:32:06 -0500

    Fix
    Updating trouble set up

--- lua/setUp/finder.lua
+++ lua/setUp/finder.lua
@@ -1,12 +1,12 @@
-local trouble = require("trouble.providers.telescope")
+local trouble = require("trouble.sources.telescope")
 --local actions = require("telescope.actions")
 local telescope = require("telescope")
 
 telescope.setup({
 	defaults = {
 		mappings = {
-			i = { ["<C-t>"] = trouble.open_with_trouble },
-			n = { ["<C-e>"] = trouble.open_with_trouble },
+			i = { ["<C-t>"] = trouble.open },
+			n = { ["<C-e>"] = trouble.open },
 		},
 	},
 	pickers = {},
