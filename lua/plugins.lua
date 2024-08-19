1f2e6ff43aaff25518bab13fff7ade161868ad81
Andres M. Prieto <andresprieto2703@gmail.com>
2024-08-19 17:32:59 -0500

    Moving configuration to pckr.nvim
    Unfortunately packer.nvim is not longer being active, so I'm moving everything to pckr.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,170 +1,183 @@
-vim.cmd([[packadd packer.nvim]])
+local function bootstrap_pckr()
+  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
 
-return require("packer").startup(function(use)
-  -- PLUGIN MANAGER
-  use("wbthomason/packer.nvim")
+  if not vim.uv.fs_stat(pckr_path) then
+    vim.fn.system({
+      'git',
+      'clone',
+      "--filter=blob:none",
+      'https://github.com/lewis6991/pckr.nvim',
+      pckr_path
+    })
+  end
 
+  vim.opt.rtp:prepend(pckr_path)
+end
+
+bootstrap_pckr()
+
+
+require('pckr').add{
   -- DIAGNOSTICS
-  use("folke/lsp-colors.nvim")
-  use({
+  "folke/lsp-colors.nvim",
+  {
     "folke/trouble.nvim",
     requires = "nvim-tree/nvim-web-devicons",
-  })
+  },
 
   -- Ollama
-  use("David-Kunz/gen.nvim")
+  "David-Kunz/gen.nvim",
 
   -- LSP
-  use("neovim/nvim-lspconfig")
-  use("mfussenegger/nvim-dap")
-  use("mfussenegger/nvim-lint")
-  use("williamboman/mason.nvim")
-  use("mhartington/formatter.nvim")
-  use("williamboman/mason-lspconfig.nvim")
+  "neovim/nvim-lspconfig",
+  "mfussenegger/nvim-dap",
+  "mfussenegger/nvim-lint",
+  "williamboman/mason.nvim",
+  "mhartington/formatter.nvim",
+  "williamboman/mason-lspconfig.nvim",
 
   -- MODES
-  use("Iron-E/nvim-libmodal")
+  "Iron-E/nvim-libmodal",
 
   -- START SCREEN
-  use({
+  {
     "goolord/alpha-nvim",
     requires = {
       "nvim-lua/plenary.nvim",
       "kyazdani42/nvim-web-devicons",
       "nvim-telescope/telescope.nvim",
     },
-  })
+  },
 
   -- THEME
   -- Status line
-  use({
+  {
     "nvim-lualine/lualine.nvim",
     requires = {
       "kyazdani42/nvim-web-devicons",
-      opt = true,
     },
-  })
+  },
 
   -- Nvim
-  use("norcalli/nvim-colorizer.lua")
-  use("marko-cerovac/material.nvim")
+  "norcalli/nvim-colorizer.lua",
+  "marko-cerovac/material.nvim",
 
   -- Highlight
-  use({
+  {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
-  })
+  },
 
   -- NAVIGATION
   -- File explorer
-  use({
+  {
     "kyazdani42/nvim-tree.lua",
     requires = "kyazdani42/nvim-web-devicons",
-  })
+  },
   -- Files opened
-  use("akinsho/bufferline.nvim")
+  "akinsho/bufferline.nvim",
 
   -- MOTION
   -- Navigation between windows
-  use("christoomey/vim-tmux-navigator")
+  "christoomey/vim-tmux-navigator",
   -- Navigation in files
-  use("easymotion/vim-easymotion")
+  "easymotion/vim-easymotion",
 
   --INDENTATION & SYNTAX
   -- Indentation helper (It shows the indentation of functions, etc)
-  use({
+  {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
       require("ibl").setup()
     end,
-  })
+  },
   -- TypeScript syntax
-  use("leafgarland/typescript-vim")
+  "leafgarland/typescript-vim",
   -- JS and JSX syntax
-  use("maxmellon/vim-jsx-pretty")
+  "maxmellon/vim-jsx-pretty",
   -- JavaScript support
-  use("pangloss/vim-javascript")
+  "pangloss/vim-javascript",
   -- Syntax highlight for multiple languages
-  use("sheerun/vim-polyglot")
+  "sheerun/vim-polyglot",
   -- Support to git commands
-  use("tpope/vim-fugitive")
+  "tpope/vim-fugitive",
   -- Indentation for bash scripts
-  use("z0mbix/vim-shfmt")
+  "z0mbix/vim-shfmt",
 
   -- UTILITIES
   -- Markdown preview
-  use({
+  {
     "iamcco/markdown-preview.nvim",
     run = function()
       vim.fn["mkdp#util#install"]()
     end,
-  })
+  },
 
   -- IMAGE PREVIEWER
-  use({ "m00qek/baleia.nvim", tag = "v1.2.0" })
-  use({
+  { "m00qek/baleia.nvim", tag = "v1.2.0" },
+  {
     "samodostal/image.nvim",
     requires = {
       "nvim-lua/plenary.nvim",
     },
-  })
+  },
 
   -- Live server
-  use({
+  {
     "turbio/bracey.vim",
     run = "npm install --prefix server",
     cmd = "Bracey",
-  })
+  },
 
   -- Multicursor
-  use("terryma/vim-multiple-cursors")
+  "terryma/vim-multiple-cursors",
   -- Easy way to make comments
-  use("preservim/nerdcommenter")
+  "preservim/nerdcommenter",
   -- Color picker for css
-  use("KabbAmine/vCoolor.vim")
+  "KabbAmine/vCoolor.vim",
   -- Git diffs
-  use("mhinz/vim-signify")
+  "mhinz/vim-signify",
   -- Show #fffffffff with colors
-  use("ap/vim-css-color")
+  "ap/vim-css-color",
   -- Repeat all the commands using dot key
-  use("tpope/vim-repeat")
+  "tpope/vim-repeat",
 
   -- AUTO COMPLETE
   -- It gives nvim a general editing configuration for identation
-  use("editorconfig/editorconfig-vim")
+  "editorconfig/editorconfig-vim",
   -- Auto complete parentesis
-  use("jiangmiao/auto-pairs")
+  "jiangmiao/auto-pairs",
   -- It helps to 'CRUD' parentesis, comillas and tags
-  use("tpope/vim-surround")
+  "tpope/vim-surround",
   -- Auto complete tags
-  use("windwp/nvim-ts-autotag")
+  "windwp/nvim-ts-autotag",
 
   -- SNIPPETS
-  use("tomtom/tlib_vim")
-  use("L3MON4D3/LuaSnip")
-  use("garbas/vim-snipmate")
-  use("MarcWeber/vim-addon-mw-utils")
+  "tomtom/tlib_vim",
+  "L3MON4D3/LuaSnip",
+  "garbas/vim-snipmate",
+  "MarcWeber/vim-addon-mw-utils",
 
   -- AUTO COMPLETE
-  use({
+  {
     "hrsh7th/nvim-cmp",
-    require = {
-      use("hrsh7th/cmp-git"),
-      use("hrsh7th/cmp-path"),
-      use("hrsh7th/cmp-calc"),
-      use("hrsh7th/cmp-buffer"),
-      use("hrsh7th/cmp-cmdline"),
-      use("hrsh7th/cmp-nvim-lsp"),
-      use("hrsh7th/cmp-nvim-lua"),
-      use("onsails/lspkind.nvim"),
-      use("ray-x/cmp-treesitter"),
-      use("uga-rosa/cmp-dictionary"),
-      use("saadparwaiz1/cmp_luasnip"),
-      use("hrsh7th/cmp-nvim-lsp-signature-help"),
-      use("hrsh7th/cmp-nvim-lsp-document-symbol"),
+    requires = {
+      "hrsh7th/cmp-git",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-calc",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "onsails/lspkind.nvim",
+      "ray-x/cmp-treesitter",
+      "uga-rosa/cmp-dictionary",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "hrsh7th/cmp-nvim-lsp-document-symbol",
     },
-  })
+  },
 
   -- AUTO SAVE
-  use("Pocco81/auto-save.nvim")
-end)
+  "Pocco81/auto-save.nvim"
+}
